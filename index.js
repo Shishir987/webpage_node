7c550296094a053885a457755ddf21326323cdf3
Shihsir987 <shishirgaire35@gmail.com>
2020-09-10 09:08:13 +0545

    second commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app=express();
 
-app.listen(4000, ()=>{console.log("Server started")});
+app.listen(process.env.port, ()=>{console.log("Server started")});
 
 app.get('/',(req,res)=>{res.sendFile(__dirname+"/pages/index.html")});
 
