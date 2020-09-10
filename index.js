3f50dda5a1c7761b0e248d7610ec00144bd67d49
Shihsir987 <shishirgaire35@gmail.com>
2020-09-10 15:27:06 +0545

    'logo'

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app=express();
 
 app.set('port',process.env.PORT || 8080);
 var port = app.get('port');
-app.listen(port,()=>{console.log('Servger running on port' + port)});
+app.listen(port,()=>{console.log('Server running on port' + port)});
 
 app.get('/',(req,res)=>{res.sendFile(__dirname+"/pages/index.html")});
 
